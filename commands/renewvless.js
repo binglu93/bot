6f604d0638bc6da0e8990282f759a4f5a72771c3
Julak Bantur <julak.kdg93@gmail.com>
2025-10-19 20:17:37 +0800

    Delete commands directory

--- commands/renewvless.js
+++
@@ -1,9 +0,0 @@
-// commands/renewvless.js
-const { createRenewVLPlugin } = require('../lib/renewBaseVL');
-
-module.exports = createRenewVLPlugin({
-  name: 'renewvless',
-  aliases: ['renew-vless'],
-  title: 'Perpanjang Akun VLess',
-  commandTpl: '/usr/local/sbin/bot-extvl {USER} {EXP}'
-});
